2fb9e97121cc8404e7c8408d87a29772781be187
jack <jack123@qq.com>
2022-02-05 19:43:28 +0800

    项目初始化

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/ProductMain.java
@@ -0,0 +1,11 @@
+package com.atguigu.gulimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMain.class, args);
+    }
+}
